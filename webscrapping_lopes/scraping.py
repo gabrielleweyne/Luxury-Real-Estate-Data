90908ed4c80d561a82b93d49b4d1e91382f78583
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-10-15 19:09:29 -0300

    Inserting data into database

--- webscrapping_lopes/scraping.py
+++ webscrapping_lopes/scraping.py
@@ -1,65 +1,52 @@
-import time
+from extract import get_html_soup
+from save import save_to_db
 import pandas as pd # manipulação de dados
-from bs4 import BeautifulSoup # Parseador de HTML
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 
-options = webdriver.FirefoxOptions() # Opções do webdrive
-options.add_argument("-headless") # Rodar o firefox no terminal
+lopes_url_root = 'https://www.lopes.com.br'
+listing_url = f'{lopes_url_root}/busca/venda/br/sp/sao-paulo?preco-menor=15000000&placeId=ChIJ0WGkg4FEzpQRrlsz_whLqZs&origem=search'
+listing_xpath = '/html/body/lps-root/lps-search/div/div/div/div/lps-search-grid/lps-search-content/div/div[1]/div[2]/lps-card-grid/div[1]/ul'
+svg_icon_meaning = {
+    'lps-icon-ruler': 'total area',
+    'lps-icon-bed': 'dorms',
+    'lps-icon-car': 'parking',
+    'lps-icon-sink': 'toilets'
+}
 
-driver = webdriver.Firefox(options=options) # Por default ele já procura onde o Firefox instalou e onde está o geckodriver
-driver.get("https://www.lopes.com.br/busca/venda/br/sp/sao-paulo?preco-menor=15000000&placeId=ChIJ0WGkg4FEzpQRrlsz_whLqZs&origem=search") # Bater na página
+# TODO: Fazer um loop para pegar a listagem de X páginas
+print('GETTING ESTATE LIST FROM ' + listing_url)
+soup = get_html_soup(listing_url, listing_xpath)
 
-time.sleep(5) # esperar 5 segundos para a página carregar
+# Pegar os links dos imóveis
+estate_links = list(map(lambda el: lopes_url_root + el.find('a').get('href'), soup.find_all('div', {'class': 'card ng-star-inserted'})))
 
-xpath = '/html/body/lps-root/lps-search/div/div/div/div/lps-search-grid/lps-search-content/div/div[1]/div[2]/lps-card-grid/div[1]/ul'
-el = driver.find_element(By.XPATH, xpath)
-html_ctnt = el.get_attribute('outerHTML')
+estates_datas = []
 
-driver.quit() # fechar o navegador
+# Extrair os dados dos imóveis das suas respectivas páginas de detalhe
+for link in estate_links:
+    estate_data = {'source_id': link.split('/')[4]}
 
-soup = BeautifulSoup(html_ctnt, 'lxml')
-
-items = []
-
-for div in soup.find_all('div', {'class': 'card ng-star-inserted'}):
-    imovel = {}
-
-    estate_info_div = div.find('div', {'class': 'card__textbox_container'})
-    # print('estate_info_div:', estate_info_div)
-    imovel['preco'] = estate_info_div.find('h4', {'class': 'card__price ng-star-inserted'}).string
+    print('GETTING INFO ON', estate_data['source_id'])
+    estate_info_html = get_html_soup(link, '/html/body/lps-root/lps-product-layout/nav-layout-default/lps-product/div[1]/div[1]/div[1]/div')
     
-    location_ps = estate_info_div.find_all('p', {'class': 'card__location'})
-    imovel['localizacao'] = f'{location_ps[0].string} - {location_ps[1].string}'
+    estate_data['address'] = estate_info_html.find('lps-product-address').div.p.string
+    
+    estate_data['price'] = estate_info_html.find('lps-product-price').find('p', {'class': 'product-price__body ng-star-inserted'}).string
+    
+    for attr_li in estate_info_html.find_all('li', {'class': 'product-attribute ng-star-inserted'}):
+         svg_icon = attr_li.div.find('lps-icon').div.find_all_next()[0].name
+         key = svg_icon_meaning.get(svg_icon)
+         if key != None:
+            # TODO: Fazer uma Regex para extrair apenas os números dos valores
+            estate_data[key] = attr_li.find('div', {'class': 'prod uct-attribute__info__value'}).string
 
-    metrics = div.find('ul', {'class': 'attributes'}).find_all('li', {'class', 'attributes__icon-wrapper ng-star-inserted'})
+    estates_datas.append(estate_data)
+    print('FINISHED', estate_data['source_id'], ' ==> ', estate_data)
 
-    imovel['tags'] = []
+# Transformar em um dataframe
+df = pd.DataFrame(estates_datas)
 
-    for m in metrics:
-        # lps-icon-ruler -> área total
-        # lps-icon-bed -> dormitórios
-        # lps-icon-car -> vagas
-        # lps-icon-sink -> banheiros
-        tag = {}
-        tag['valor'] = m.find('div', {'class.attributes__value': 'withLabel'}).string
-        
-        if m.find('lps-icon-ruler') != None:
-            tag['significado'] =  'área total'
-        elif m.find('lps-icon-bed') != None:
-            tag['significado'] =  'dormitórios'
-        elif m.find('lps-icon-car') != None:
-            tag['significado'] =  'vagas'
-        elif m.find('lps-icon-sink') != None:
-            tag['significado'] =  'banheiros'
-        else:
-            tag['significado'] =  'desconhecido'
-        imovel['tags'].append(tag)
+# Criar um csv para usar durante o desenvolvimento
+df.to_csv("lopes_imoveis.csv")
+# df = pd.read_csv('lopes_imoveis.csv')
 
-    print('Scrapping', imovel)
-    # items.append(li)
-
-# print(items)
-# df_full = pd.read_html(str(table))[0] # já transforma a table em um data frame com os nomes dos dados
-# # print(df_full)
-# df_full.to_csv("lopes_imoveis.csv")
+save_to_db(df)
