c52d983b447da97d05a65b1a9a152702870961be
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-10-30 10:15:46 -0300

    Refactored dev env code

--- webscrapping_lopes/save.py
+++ webscrapping_lopes/save.py
@@ -9,9 +9,13 @@ SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/scrap_estate
 def save_to_db(estates, page=1):
     # Transformar em um dataframe
     df = pd.DataFrame(estates)
+
     # Criar um csv para usar durante o desenvolvimento
     df.to_csv(f"lopes_imoveis{page}.csv")
-    # df = pd.read_csv(f"lopes_imoveis{page}.csv")
+
+    # Ler o CSV
+    # df = pd.read_csv("arquivo.csv")
+    # df.drop(columns=["Unnamed: 0"], inplace=True)
 
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -20,9 +24,6 @@ def save_to_db(estates, page=1):
     session = SessionLocal()
     Base = declarative_base()
     Base.metadata.create_all(bind=engine)
-
-    # Apenas para quando ler do csv
-    # estates_df.drop(columns=['Unnamed: 0'], inplace=True)
 
     # Colocar os dados no SQL
     print("========= Loading into database...")
