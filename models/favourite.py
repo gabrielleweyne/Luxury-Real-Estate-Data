7b528e510beeb32e0b14468f1857297b60723440
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-12-08 10:02:12 -0300

    Fixed favourite unique constraint

--- models/favourite.py
+++ models/favourite.py
@@ -1,15 +1,16 @@
 from . import Base
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
 from sqlalchemy.orm import relationship
 
 
 class Favourite(Base):
     __tablename__ = "favourites"
-    user_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE'), unique=True, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE'), primary_key=True)
     user = relationship("User")
-    estates_ind_id = Column(Integer, ForeignKey("estates_ind.id"), unique=True, primary_key=True)
+    estates_ind_id = Column(Integer, ForeignKey("estates_ind.id"), primary_key=True)
     estates_ind = relationship("EstatesInd")
     favourited = Column(Integer, default=0)
+    UniqueConstraint(user_id, estates_ind_id)
 
     def to_view(self):
         return {
